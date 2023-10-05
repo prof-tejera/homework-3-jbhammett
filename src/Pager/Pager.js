09f41089c8ba57c8e7ba89f3ceb52d0ac7cc8b9d
jbhammett <jbeakle@g.clemson.edu>
2023-10-05 01:16:10 -0400

    Implemented and styled components.

--- src/Pager/Pager.js
+++ src/Pager/Pager.js
@@ -1,7 +1,25 @@
 import "./Pager.css";
+import Button from 'components/Button/Button';
+import Panel from 'components/Panel/Panel';
 
 const Pager = () => {
-  return null;
+  return <>
+    <Panel>
+
+      <Button active="True" text="<" />
+      <Button active="True" text="1" />
+      <Button active="True" text="2"/>
+      <Button active="True" text="3"/>
+      <Button active="True" text="4"/>
+      <Button active="True" text="5"/>
+      <Button active="True" text="6"/>
+      <Button active="True" text="7"/>
+      <Button active="True" text="8"/>
+      <Button active="True" text="9"/>
+      <Button active="True" text="10"/>
+      <Button active="True" text=">"/>
+    </Panel>
+  </>
 };
 
 export default Pager;
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Panel = (props) => {
+  
   return <div>{props.children}</div>;
 };
 
